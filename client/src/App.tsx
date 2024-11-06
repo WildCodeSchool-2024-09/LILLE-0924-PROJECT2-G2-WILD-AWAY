5648461392d927c07599fced19edd87e587d6ca2
ALICIA <alicia.magro@gmail.com>
2024-11-06 17:10:51 +0100

    about us is done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
+import AboutUs from "./pages/AboutUs/AboutUs";
 
 function App() {
   return (
     <>
       <NavBar />
+      <AboutUs />
     </>
   );
 }
