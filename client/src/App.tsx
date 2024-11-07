f43cb5d1b5afa08b8f160d2960688717978427b2
ALICIA <alicia.magro@gmail.com>
2024-11-07 11:13:14 +0100

    trying to add the router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
