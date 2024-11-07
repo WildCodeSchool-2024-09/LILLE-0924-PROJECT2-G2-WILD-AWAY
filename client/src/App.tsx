c930bb1a440899d99b6b91968381719394556a2f
ALICIA <alicia.magro@gmail.com>
2024-11-07 14:42:12 +0100

    finalised the nevbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,13 @@ import "./App.css";
 
 import NavBar from "./components/NavBar/NavBar";
 
-import { Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <Outlet />
     </>
   );
 }
