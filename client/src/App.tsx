0646009b39f47fd89c37c5128e02f97d3bde178c
Arnaud6216 <guevaer8@gmail.com>
2024-11-07 17:08:13 +0100

    adding Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,13 @@ import "./App.css";
 
 import NavBar from "./components/NavBar/NavBar";
 
-import { Route, Routes } from "react-router-dom";
+import Booking from "./pages/booking/Booking";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <Booking />
     </>
   );
 }
