85615624bf927aae8632585631ff4758d48e2d2a
Arnaud6216 <guevaer8@gmail.com>
2024-11-19 12:24:14 +0100

    inversing carousels

--- client/src/pages/booking/Booking.tsx
+++ client/src/pages/booking/Booking.tsx
@@ -38,60 +38,6 @@ function Booking() {
         <>
           <h1>{bookingData.city}</h1>
           <main className="booking-main">
-            <div className="slider">
-              <div className="slide-track">
-                <div className="slide">
-                  <img
-                    src="../public/slide1.jpg"
-                    className="img-slide"
-                    alt="slide1"
-                  />
-                </div>
-                <div className="slide">
-                  <img
-                    src="../public/slide2.jpg"
-                    className="img-slide"
-                    alt="slide2"
-                  />
-                </div>
-                <div className="slide">
-                  <img
-                    src="../public/slide3.jpg"
-                    className="img-slide"
-                    alt="slide3"
-                  />
-                </div>
-                <div className="slide">
-                  <img
-                    src="../public/slide4.jpg"
-                    className="img-slide"
-                    alt="slide4"
-                  />
-                </div>
-                <div className="slide">
-                  <img
-                    src="../public/slide5.jpg"
-                    className="img-slide"
-                    alt="slide5"
-                  />
-                </div>
-                <div className="slide">
-                  <img
-                    src="../public/slide6.jpg"
-                    className="img-slide"
-                    alt="slide6"
-                  />
-                </div>
-              </div>
-            </div>
-            <section>
-              <HotelCard bookingData={bookingData} />
-            </section>
-
-            <section>
-              <RestaurantCard bookingData={bookingData} />
-            </section>
-
             <div className="sliders">
               <div className="slides-track">
                 <div className="slides">
@@ -134,6 +80,61 @@ function Booking() {
                     src="../public/slide12.jpg"
                     className="img-slide"
                     alt="slide12"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <section>
+              <HotelCard bookingData={bookingData} />
+            </section>
+
+            <section>
+              <RestaurantCard bookingData={bookingData} />
+            </section>
+
+            <div className="slider">
+              <div className="slide-track">
+                <div className="slide">
+                  <img
+                    src="../public/slide1.jpg"
+                    className="img-slide"
+                    alt="slide1"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide2.jpg"
+                    className="img-slide"
+                    alt="slide2"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide3.jpg"
+                    className="img-slide"
+                    alt="slide3"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide4.jpg"
+                    className="img-slide"
+                    alt="slide4"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide5.jpg"
+                    className="img-slide"
+                    alt="slide5"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide6.jpg"
+                    className="img-slide"
+                    alt="slide6"
                   />
                 </div>
               </div>
