bb43960966aed8e497e3d371f69ec7595998cf9b
Arnaud6216 <guevaer8@gmail.com>
2024-11-19 11:53:53 +0100

    adding carousels to booking

--- client/src/pages/booking/Booking.tsx
+++ client/src/pages/booking/Booking.tsx
@@ -38,6 +38,52 @@ function Booking() {
         <>
           <h1>{bookingData.city}</h1>
           <main className="booking-main">
+            <div className="slider">
+              <div className="slide-track">
+                <div className="slide">
+                  <img
+                    src="../public/slide1.jpg"
+                    className="img-slide"
+                    alt="slide1"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide2.jpg"
+                    className="img-slide"
+                    alt="slide2"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide3.jpg"
+                    className="img-slide"
+                    alt="slide3"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide4.jpg"
+                    className="img-slide"
+                    alt="slide4"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide5.jpg"
+                    className="img-slide"
+                    alt="slide5"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide6.jpg"
+                    className="img-slide"
+                    alt="slide6"
+                  />
+                </div>
+              </div>
+            </div>
             <section>
               <HotelCard bookingData={bookingData} />
             </section>
@@ -45,6 +91,53 @@ function Booking() {
             <section>
               <RestaurantCard bookingData={bookingData} />
             </section>
+
+            <div className="slider">
+              <div className="slide-track">
+                <div className="slide">
+                  <img
+                    src="../public/slide7.jpg"
+                    className="img-slide"
+                    alt="slide7"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide8.jpg"
+                    className="img-slide"
+                    alt="slide8"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide9.jpg"
+                    className="img-slide"
+                    alt="slide9"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide10.jpg"
+                    className="img-slide"
+                    alt="slide10"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide11.jpg"
+                    className="img-slide"
+                    alt="slide11"
+                  />
+                </div>
+                <div className="slide">
+                  <img
+                    src="../public/slide12.jpg"
+                    className="img-slide"
+                    alt="slide12"
+                  />
+                </div>
+              </div>
+            </div>
           </main>
         </>
       )}
