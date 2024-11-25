8a5e42487f38538ba325d2a6b8ad4d55adabf05d
Arnaud6216 <guevaer8@gmail.com>
2024-11-25 11:45:02 +0100

    fix

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -5,22 +5,17 @@ function Footer() {
     <footer>
       <section className="footer">
         <div className="reseau">
-          <div className="twitter">
-            <img src="/twitter.png" className="logo" alt="logo de Twitter" />{" "}
-            @WildAway <br />
-          </div>
-          <div className="instagram">
-            <img
-              src="/instagram.png"
-              className="logo"
-              alt="logo de Instagram"
-            />{" "}
-            @WildAway
-          </div>
+          <img src="/twitter.png" className="logo" alt="logo de Twitter" />{" "}
+          @WildAway <br />
+          <img src="/instagram.png" className="logo" alt="logo de Instagram" />{" "}
+          @WildAway
         </div>
         <div className="contact">
           <p>© WildAway 2024 </p>
-          <p className="contact-us">Nous contacter</p>
+        </div>
+        <div>
+          {" "}
+          <p>Nous contacter</p>
         </div>
       </section>
     </footer>
